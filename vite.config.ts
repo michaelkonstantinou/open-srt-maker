e5d30bd1d177319c027718a895d5b7cabac127ff
Michael Konstantinou <mikhailkonstan@hotmail.com>
2025-08-21 13:27:53 +0200

    Updated: Routes now support github-pages convention

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/open-srt-maker/', // Replace with your GitHub repository name
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
